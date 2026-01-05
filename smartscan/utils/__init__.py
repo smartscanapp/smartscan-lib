ccda385601b7ad6f2c267e2e4b512b645ba4f591
d41dev <d41dev@proton.me>
2026-01-05 13:36:49 +0000

    update: video utils export

--- smartscan/utils/__init__.py
+++ smartscan/utils/__init__.py
@@ -1,2 +1,3 @@
-from smartscan.utils.file_utils import read_text_file, get_days_since_last_modified, get_child_dirs, get_files_from_dirs, get_frames_from_video, are_valid_files
+from smartscan.utils.file_utils import read_text_file, get_days_since_last_modified, get_child_dirs, get_files_from_dirs, are_valid_files
+from smartscan.utils.video_utils import get_frames_from_video, get_video_metadata
 from smartscan.utils.image_utils import nms, draw_boxes, crop_faces
