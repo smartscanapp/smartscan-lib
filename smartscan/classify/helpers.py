dd2547d357fae5d3eab39015e54d0585ff98f4d4
d41dev <d41dev@proton.me>
2026-01-10 21:19:21 +0000

    update: use new cluster cluster metadata types

--- smartscan/classify/helpers.py
+++ smartscan/classify/helpers.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 from typing import Dict
-from smartscan.classify.types import BaseCluster, ClusterMerges
+from smartscan.classify.types import Cluster, ClusterMerges
 
 
-def merge_similar_clusters(cluster_prototypes: Dict[str, BaseCluster], merge_threshold: float = 0.9,verbose: bool = False) -> ClusterMerges:
+def merge_similar_clusters(cluster_prototypes: Dict[str, Cluster], merge_threshold: float = 0.9,verbose: bool = False) -> ClusterMerges:
     cluster_merges: ClusterMerges = {}
     cluster_ids = list(cluster_prototypes.keys())
     cluster_embeds = [p.embedding for p in cluster_prototypes.values()]
