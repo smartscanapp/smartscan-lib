7ed636836fdc706391f158c3579e26a5d81183b3
d41dev <d41dev@proton.me>
2026-02-04 16:41:11 +0000

    update: add model types

--- smartscan/types.py
+++ smartscan/types.py
@@ -1,13 +1,18 @@
-from typing import Literal
+from typing import Literal, TypeAlias, TypedDict, Optional, List
 from smartscan.embeds.types import *
 from smartscan.classify.types import *
 from smartscan.media.types import VideoSource, ImageSource
 from smartscan.providers import TextEmbeddingProvider, ImageEmbeddingProvider, EmbeddingProvider
 
-ModelName = Literal[
-    "clip-vit-b-32-image",
-    "clip-vit-b-32-text",
-    "dinov2-small",
-    "inception-resnet-v1",
-    "all-minilm-l6-v2",
-]
+
+LocalTextEmbeddingModel: TypeAlias = Literal["all-minilm-l6-v2", "clip-vit-b-32-text"]
+LocalImageEmbeddingModel: TypeAlias = Literal["clip-vit-b-32-image", "dinov2-small"]
+LocalFaceEmbeddingModel: TypeAlias = Literal["inception-resnet-v1"]
+
+ModelName = Literal[LocalTextEmbeddingModel,LocalImageEmbeddingModel, LocalFaceEmbeddingModel]
+
+class ModelInfo(TypedDict):
+    url: str
+    path: str
+    dependencies_paths: Optional[List[str]] = None
+    file_hash: Optional[str] = None
